82a5cf54105af1f6247e142e2ed589349421c818
ilathem <ilathem@users.noreply.github.com>
2022-11-07 22:27:41 +0000

    Prettified Code!

--- src/pages/DataSheets.js
+++ src/pages/DataSheets.js
@@ -2,11 +2,9 @@ import { useState } from 'react';
 import db from '../utils/firebase';
 
 export default function DataSheets() {
-  const [ site , setSite ] = useState()
-  const [ array, setArray ] = useState()
-  const [ recorder, setRecorde ] = useState()
-  const [ handler, setHandler ] = useState()
-  const [ noCaptures, setNoCaptures ] = useState()
-
-  
+    const [site, setSite] = useState();
+    const [array, setArray] = useState();
+    const [recorder, setRecorde] = useState();
+    const [handler, setHandler] = useState();
+    const [noCaptures, setNoCaptures] = useState();
 }
--- src/pages/HerpSheet.js
+++ src/pages/HerpSheet.js
@@ -1,14 +1,12 @@
 import { useState } from 'react';
 
 export default function HerpSheet({ submitSingleCapture }) {
-  const [ fenceTrap, setFenceTrap ] = useState()
-  const [ speciesCode, setSpeciesCode ] = useState()
-  const [ recapture, setRecapture ] = useState()
-  const [ svl, setSvl ] = useState()
-  const [ vtl, setVtl ] = useState()
-  const [ mass, setMass ] = useState()
-  const [ sex, setSex ] = useState()
-  const [ notes, setNotes ] = useState()
-
-  
+    const [fenceTrap, setFenceTrap] = useState();
+    const [speciesCode, setSpeciesCode] = useState();
+    const [recapture, setRecapture] = useState();
+    const [svl, setSvl] = useState();
+    const [vtl, setVtl] = useState();
+    const [mass, setMass] = useState();
+    const [sex, setSex] = useState();
+    const [notes, setNotes] = useState();
 }
