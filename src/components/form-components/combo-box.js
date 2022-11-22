89ef9e872c458825d0e7f22c48cb81d9e216a9ef
realdgrassl <dgrassl@asu.edu>
2022-11-22 17:31:08 -0500

    Update combo-box component

--- src/components/form-components/combo-box.js
+++ src/components/form-components/combo-box.js
@@ -1,32 +1,15 @@
 import React from 'react';
 import { Form, FormGroup, FormLabel } from 'react-bootstrap';
 
-const Combobox = ({ prompt, onChange, options, disabled, defValue }) => {
-    return (
-        <FormGroup controlId={prompt}>
-            <FormLabel className="select-group">
-                <span>{prompt}</span>
-                <Form.Control required as="select" custom onChange={onChange} disabled={disabled}>
-                    {
-                        <option key="empty" className="list-item">
-                            {' '}
-                        </option>
-                    }
-                    {options.map((option) => {
-                        return (
-                            <option
-                                key={option}
-                                className="list-item"
-                                selected={option === defValue}
-                            >
-                                {option}
-                            </option>
-                        );
-                    })}
-                </Form.Control>
-            </FormLabel>
-        </FormGroup>
-    );
-};
-
-export default Combobox;
+export default ({ prompt, onChange, options, disabled, defValue }) => (
+    <FormGroup controlId={prompt}>
+        <FormLabel className="select-group">
+            <span>{prompt}</span>
+            <Form.Control required as="select" custom onChange={onChange} disabled={disabled} defaultValue={defValue}>
+                {[' ', ...options].map(option =>
+                    <option key={option == ' ' ? 'empty' : option} className="list-item">{option}</option>
+                )}
+            </Form.Control>
+        </FormLabel>
+    </FormGroup>
+);
