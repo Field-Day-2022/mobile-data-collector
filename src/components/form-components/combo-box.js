68a02c95959b35ddb973f5dd54781644720f1451
realdgrassl <realdgrassl@users.noreply.github.com>
2022-11-30 19:10:21 +0000

    Prettified Code!

--- src/components/form-components/combo-box.js
+++ src/components/form-components/combo-box.js
@@ -5,10 +5,19 @@ export default ({ prompt, onChange, options, disabled, defValue }) => (
     <FormGroup controlId={prompt}>
         <FormLabel className="select-group">
             <span>{prompt}</span>
-            <Form.Control required as="select" custom onChange={onChange} disabled={disabled} defaultValue={defValue}>
-                {[' ', ...options].map(option =>
-                    <option key={option == ' ' ? 'empty' : option} className="list-item">{option}</option>
-                )}
+            <Form.Control
+                required
+                as="select"
+                custom
+                onChange={onChange}
+                disabled={disabled}
+                defaultValue={defValue}
+            >
+                {[' ', ...options].map((option) => (
+                    <option key={option == ' ' ? 'empty' : option} className="list-item">
+                        {option}
+                    </option>
+                ))}
             </Form.Control>
         </FormLabel>
     </FormGroup>
