f7c270c46b8ea157aac5bff37e29252d91b59c2c
ilathem <isaiahlathem@gmail.com>
2023-02-15 17:38:56 -0600

    Finish TS282 dynamic answer set loading

--- src/forms/NewArthropodEntry.jsx
+++ src/forms/NewArthropodEntry.jsx
@@ -1,6 +1,5 @@
-import { useState } from 'react';
-import { useAtom } from 'jotai'
-
+import { useEffect, useState } from 'react';
+import { useAtom } from 'jotai';
 import NumberInput from '../components/NumberInput';
 import Dropdown from '../components/Dropdown';
 import FormWrapper from '../components/FormWrapper';
@@ -8,90 +7,114 @@ import SingleCheckbox from '../components/SingleCheckbox';
 import TextInput from '../components/TextInput';
 import Button from '../components/Button';
 import ConfirmationModal from '../components/ConfirmationModal';
-
 import { currentFormName, currentSessionData } from '../utils/jotai';
-
 import { updateData } from '../utils/functions';
+import {
+    collection,
+    setDoc,
+    query,
+    where,
+    doc,
+    getDocsFromCache,
+    getDocFromCache,
+} from 'firebase/firestore';
+import { db } from '../index';
 
-import { 
-  arthropodSpeciesList as species, 
-  arthropodFenceTraps as fenceTraps
+import {
+    // arthropodSpeciesList as species,
+    // arthropodFenceTraps as fenceTraps,
 } from '../utils/hardCodedData';
 
 export default function NewArthropodEntry() {
-  const [trap, setTrap] = useState();
-  const [predator, setPredator] = useState(false);
-  const [arthropodData, setArthropodData] = useState();
-  const [comments, setComments] = useState('')
-  const [ confirmationModalIsOpen, setConfirmationModalIsOpen ] = useState(false);
+    const [trap, setTrap] = useState();
+    const [predator, setPredator] = useState(false);
+    const [arthropodData, setArthropodData] = useState();
+    const [comments, setComments] = useState('');
+    const [confirmationModalIsOpen, setConfirmationModalIsOpen] = useState(false);
+    const [arthropodSpeciesList, setArthropodSpeciesList] = useState([]);
+    const [fenceTraps, setFenceTraps] = useState([]);
 
-  const [currentData, setCurrentData] = useAtom(currentSessionData)
-  const [currentForm, setCurrentForm] = useAtom(currentFormName);
+    const [currentData, setCurrentData] = useAtom(currentSessionData);
+    const [currentForm, setCurrentForm] = useAtom(currentFormName);
 
-  // todo: input validation
-  // todo: dynamic answer set loading
+    // todo: input validation
 
-  const completeCapture = () => {
-    const date = new Date()
-    updateData(
-      'arthropod',
-      {
-        trap,
-        predator,
-        arthropodData,
-        comments,
-        dateTime: date.toUTCString(),
-      },
-      setCurrentData,
-      currentData,
-      setCurrentForm
-    )
-  }
+    useEffect(() => {
+        const getAnswerFormDataFromFirestore = async () => {
+            const speciesSnapshot = await getDocsFromCache(
+                query(
+                    collection(db, 'AnswerSet'),
+                    where('set_name', '==', 'arthropodSpeciesList')
+                )
+            )
+            setArthropodSpeciesList(speciesSnapshot.docs[0].data().arthropodSpeciesArray);
+            const fenceTrapsSnapshot = await getDocsFromCache(
+                query(collection(db, 'AnswerSet'), where('set_name', '==', 'Fence Traps'))
+            );
+            let fenceTrapsArray = [];
+            for (const answer of fenceTrapsSnapshot.docs[0].data().answers) {
+                fenceTrapsArray.push(answer.primary);
+            }
+            setFenceTraps(fenceTrapsArray);
+        }
+        getAnswerFormDataFromFirestore();
+    }, [])
 
-  return (
-    <FormWrapper>
-      <Dropdown 
-        value={trap}
-        setValue={setTrap}
-        placeholder="Fence Trap"
-        options={fenceTraps}
-      />
-      <SingleCheckbox 
-        prompt="Predator?"
-        value={predator}
-        setValue={setPredator}
-      />
-      {species.map((item) => (
-        <NumberInput
-          key={item}
-          label={item}
-          placeholder='# of critters'
-          setValue={(value) =>
-            setArthropodData({ ...arthropodData, [item]: value })
-          }
-        />
-      ))}
-      <TextInput 
-        prompt="Comments"
-        placeholder="any thoughts?"
-        value={comments}
-        setValue={setComments}
-      />
-      <Button 
-        prompt="Finished?"
-        clickHandler={() => setConfirmationModalIsOpen(true)}
-      />
-      {confirmationModalIsOpen && <ConfirmationModal 
-        data={{
-          trap,
-          predator,
-          arthropodData,
-          comments,
-        }}
-        completeCapture={completeCapture}
-        setConfirmationModalIsOpen={setConfirmationModalIsOpen}
-        modalType="arthropod"
-      />}
-    </FormWrapper>
-  );
+
+    const completeCapture = () => {
+        const date = new Date();
+        updateData(
+            'arthropod',
+            {
+                trap,
+                predator,
+                arthropodData,
+                comments,
+                dateTime: date.toUTCString(),
+            },
+            setCurrentData,
+            currentData,
+            setCurrentForm
+        );
+    };
+
+    return (
+        <FormWrapper>
+            <Dropdown
+                value={trap}
+                setValue={setTrap}
+                placeholder="Fence Trap"
+                options={fenceTraps}
+            />
+            <SingleCheckbox prompt="Predator?" value={predator} setValue={setPredator} />
+            {arthropodSpeciesList.map((item) => (
+                <NumberInput
+                    key={item}
+                    label={item}
+                    placeholder="# of critters"
+                    setValue={(value) => setArthropodData({ ...arthropodData, [item]: value })}
+                />
+            ))}
+            <TextInput
+                prompt="Comments"
+                placeholder="any thoughts?"
+                value={comments}
+                setValue={setComments}
+            />
+            <Button prompt="Finished?" clickHandler={() => setConfirmationModalIsOpen(true)} />
+            {confirmationModalIsOpen && (
+                <ConfirmationModal
+                    data={{
+                        trap,
+                        predator,
+                        arthropodData,
+                        comments,
+                    }}
+                    completeCapture={completeCapture}
+                    setConfirmationModalIsOpen={setConfirmationModalIsOpen}
+                    modalType="arthropod"
+                />
+            )}
+        </FormWrapper>
+    );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,16 +5,16 @@ import { signOut } from 'firebase/auth';
 import { motion } from 'framer-motion';
 
 export default function Home() {
-    const [ user, loading, error ] = useAuthState(auth);
-
-    console.log(user);
-
+    const [user, loading, error] = useAuthState(auth);
     return (
         <motion.div>
             <motion.h1 className="text-xl">Hello, {user.displayName}!</motion.h1>
-            <button className="text-lg px-4 py-1 border-[1px] border-asu-maroon rounded-xl my-2"
+            <button
+                className="text-lg px-4 py-1 border-[1px] border-asu-maroon rounded-xl my-2"
                 onClick={() => signOut(auth)}
-            >Logout</button>
+            >
+                Logout
+            </button>
         </motion.div>
     );
 }
