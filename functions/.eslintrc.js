a4e0a4a4bb0bc29c1d3f2fdda5e5d937117cdfc1
ilathem <ilathem@users.noreply.github.com>
2023-03-27 05:05:51 +0000

    Prettified Code!

--- functions/.eslintrc.js
+++ functions/.eslintrc.js
@@ -1,14 +1,11 @@
 module.exports = {
-  root: true,
-  env: {
-    es6: true,
-    node: true,
-  },
-  extends: [
-    "eslint:recommended",
-    "google",
-  ],
-  rules: {
-    quotes: ["error", "double"],
-  },
+    root: true,
+    env: {
+        es6: true,
+        node: true,
+    },
+    extends: ['eslint:recommended', 'google'],
+    rules: {
+        quotes: ['error', 'double'],
+    },
 };
