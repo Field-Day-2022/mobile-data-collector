606005dffc5c7646d1619042b35194f7bedeff96
ilathem <isaiahlathem@gmail.com>
2023-03-28 22:14:38 -0500

    worked on backend some more

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,13 @@
 /* eslint-disable no-undef */
-const firestore = require('firebase-admin');
+const {initializeApp} = require('firebase-admin/app');
+const {getFirestore} = require('firebase-admin/firestore');
 const functions = require('firebase-functions');
 const cors = require('cors')({
   origin: true,
 });
+
+initializeApp();
+const db = getFirestore();
 
 // // Create and deploy your first functions
 // // https://firebase.google.com/docs/functions/get-started
@@ -16,11 +20,18 @@ const cors = require('cors')({
 exports.createBundle = functions.https.onRequest(async (request, response) => {
   cors(request, response, async () => {
     try {
-        
-    //   const gatewayLizardData = await db.
-        //   .collection('GatewayData')
-        //   .where('taxa', '==', 'Lizard')
-        //   .get();
+    //   response.send(await admin.firestore().listCollections());
+
+      //   response.send(await db.collection('GatewayData').limit(1).get());
+
+      response.send(await db.getAll(
+          db.collection('GatewayData').doc('04kK9qB8N7LHAaXQyzxz'),
+      ));
+
+      //   const gatewayLizardData = await firestore
+      //       .collection('GatewayData')
+      //       .where('taxa', '==', 'Lizard')
+      //       .get();
       //   const sanPedroLizardData = await firestore
       //       .collection('SanPedroData')
       //       .where('taxa', '==', 'Lizard')
@@ -29,28 +40,28 @@ exports.createBundle = functions.https.onRequest(async (request, response) => {
       //       .collection('VirginRiverData')
       //       .where('taxa', '==', 'Lizard')
       //       .get();
-      //   const answerSetData = await db.collection('AnswerSet').get();
+      //   const answerSetData = await firestore.collection('AnswerSet').get();
 
       //   const bundleBuffer = firestore
       //       .bundle('initial-dataset')
       //       .add('gateway-lizard-data-query', gatewayLizardData)
-      //   //   .add('sanPedro-lizard-data-query', sanPedroLizardData)
-      //   //   .add('virginRiver-lizard-data-query', virginRiverLizardData)
-      //   //   .add('answerSet-query', answerSetData)
+      //       .add('sanPedro-lizard-data-query', sanPedroLizardData)
+      //       .add('virginRiver-lizard-data-query', virginRiverLizardData)
+      //       .add('answerSet-query', answerSetData)
       //       .build();
 
-      const bundle = db.bundle('initial-dataset');
+      //   const bundle = db.bundle('initial-dataset');
 
-      const bundleBuffer = bundle.add(gatewayLizardData).build();
+      //   const bundleBuffer = bundle.add(gatewayLizardData).build();
 
-      response
-          .set('Cache-Control', 'public: max-age=7776000, s-maxage=15552000');
+      //   response
+      //       .set('Cache-Control', 'public: max-age=7776000, s-maxage=15552000');
 
-      response.set('Access-Control-Allow-Origin', '*');
+      //   response.set('Access-Control-Allow-Origin', '*');
 
-      response.end(bundleBuffer);
+      //   response.end(bundleBuffer);
 
-      response.status(200).send();
+    //   response.status(200).send();
     } catch (e) {
       console.error(e);
     }
