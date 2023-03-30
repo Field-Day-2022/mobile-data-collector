de7e2d3f6e5deb73ce569471702e5c7ee4c3f721
realdgrassl <dgrassl@asu.edu>
2023-03-30 16:45:09 -0400

    Update registerRoute

--- src/service-worker.js
+++ src/service-worker.js
@@ -27,22 +27,10 @@ precacheAndRoute(self.__WB_MANIFEST);
 const fileExtensionRegexp = new RegExp('/[^/?]+\\.[^/]+$');
 registerRoute(
     // Return false to exempt requests from being fulfilled by index.html.
-    ({ request, url }) => {
-        // If this isn't a navigation, skip.
-        if (request.mode !== 'navigate') {
-            return false;
-        } // If this is a URL that starts with /_, skip.
-
-        if (url.pathname.startsWith('/_')) {
-            return false;
-        } // If this looks like a URL for a resource, because it contains // a file extension, skip.
-
-        if (url.pathname.match(fileExtensionRegexp)) {
-            return false;
-        } // Return true to signal that we want to use the handler.
-
-        return true;
-    },
+    ({ request, url }) =>
+        request.mode === 'navigate' && // If this isn't a navigation, skip.
+        !url.pathname.startsWith('/_') && // If this is a URL that starts with /_, skip.
+        !url.pathname.match(fileExtensionRegexp), // If this looks like a URL for a resource, because it contains // a file extension, skip.
     createHandlerBoundToURL(process.env.PUBLIC_URL + '/index.html')
 );
 
