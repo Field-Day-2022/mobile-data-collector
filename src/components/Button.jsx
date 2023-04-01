bd82f0f597339174aad34234e055ac2ccad3f0c4
ilathem <isaiahlathem@gmail.com>
2023-03-31 22:01:19 -0500

    change button font to semibold

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -4,7 +4,7 @@ export default function Button({
 }) {
   return (
     <button
-        className='text-xl px-6 py-2 border-[1px] border-asu-maroon rounded-lg my-2 active:scale-90 transition'
+        className='text-xl font-semibold px-6 py-2 border-[1px] border-asu-maroon rounded-lg my-2 active:scale-90 transition'
         onClick={() => clickHandler()}
         
       >
