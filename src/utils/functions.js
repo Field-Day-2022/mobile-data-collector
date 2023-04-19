c94e18efabff1054ef89971316d3b9ff399d00de
ilathem <ilathem@users.noreply.github.com>
2023-04-19 03:42:39 +0000

    Prettified Code!

--- src/utils/functions.js
+++ src/utils/functions.js
@@ -336,17 +336,16 @@ export const completeLizardCapture = async (
 };
 
 export const getStandardizedDateTimeString = (dateString) => {
-    const tempDate = new Date(dateString)
-    return `${
-        tempDate.getFullYear()
-    }/${
-        (tempDate.getMonth() + 1).toString().padStart(2, '0')
-    }/${
-        tempDate.getDate().toString().padStart(2, '0')
-    } ${tempDate.toLocaleTimeString('en-US', {
-        hourCycle: 'h24'
-    })}`
-}
+    const tempDate = new Date(dateString);
+    return `${tempDate.getFullYear()}/${(tempDate.getMonth() + 1)
+        .toString()
+        .padStart(2, '0')}/${tempDate
+        .getDate()
+        .toString()
+        .padStart(2, '0')} ${tempDate.toLocaleTimeString('en-US', {
+        hourCycle: 'h24',
+    })}`;
+};
 
 const createLizardEntry = async (currentData, dataEntry) => {
     const dataObjTemplate = {
