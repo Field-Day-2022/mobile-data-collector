6090419dabf43b29db06f15e410575c4ad98f23e
ilathem <isaiahlathem@gmail.com>
2023-05-01 19:54:39 -0500

    add sessionDateTime to lizard entry

--- src/forms/NewMammalEntry.jsx
+++ src/forms/NewMammalEntry.jsx
@@ -100,7 +100,6 @@ export default function NewMammalEntry() {
     }, []);
 
     const completeCapture = () => {
-        const date = new Date();
         updateData(
             'mammal',
             {
