7bed9885f394036f5e05807ca1f62386e843c87a
ilathem <isaiahlathem@gmail.com>
2023-05-04 17:19:38 -0500

    change redirect to popup

--- src/pages/LoginWrapper.jsx
+++ src/pages/LoginWrapper.jsx
@@ -1,5 +1,5 @@
 import { auth } from '../index';
-import { GoogleAuthProvider, signInWithRedirect } from 'firebase/auth';
+import { GoogleAuthProvider, signInWithPopup, signInWithRedirect } from 'firebase/auth';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { motion } from 'framer-motion';
 
@@ -7,8 +7,8 @@ export const LoginWrapper = ({ children }) => {
     const [user, loading, error] = useAuthState(auth);
 
     // console.log(user)
-    if (true) {
-    // if (user && user.email.slice(-7) === 'asu.edu') {
+    // if (true) {
+    if (user && user.email.slice(-7) === 'asu.edu') {
         return children;
     } else if (loading) {
         return (
@@ -32,7 +32,7 @@ export const LoginWrapper = ({ children }) => {
                 </motion.p>
                 <motion.button
                     className="text-black border-asu-maroon text-2xl w-1/2 py-2 border-2 rounded-2xl"
-                    onClick={() => signInWithRedirect(auth, new GoogleAuthProvider())}
+                    onClick={() => signInWithPopup(auth, new GoogleAuthProvider())}
                 >
                     Login
                 </motion.button>
